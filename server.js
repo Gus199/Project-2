3f2b1296bcf9ae3c8f6143ec0a986ff16b78dbcc
Gus <gus@Guss-MBP.fios-router.home>
2021-12-06 20:26:17 -0500

    testing ...

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
+const express = require("express");
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 // express middlewar:
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
-
 // Create batabase connection:
+
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening ay'));
+  app.listen(PORT, () => console.log("Now listening ay"));
 });
